0066333a761dd62f426895d4c8978268fc5df014
cds4r <32211521+0xcds4r@users.noreply.github.com>
2025-02-26 02:16:54 +1000

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -10,5 +10,5 @@ FILE_LIST := $(wildcard $(LOCAL_PATH)/*.cpp)
 
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
-LOCAL_CPPFLAGS := -w -s -fvisibility=hidden -pthread -Wall -fpack-struct=1 -O2 -std=c++14 -fexceptions
+LOCAL_CPPFLAGS := -w -s -fvisibility=default -pthread -Wall -fpack-struct=1 -O2 -std=c++17 -fexceptions
 include $(BUILD_SHARED_LIBRARY)
